a52e6fcb180f287f17ee3b207b662d601490b8e4
rackilea <lea.racki@fer.hr>
2016-11-06 19:52:07 +0100

    Add files via upload

---
+++ zad6/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            LongOperation("A");
+            LongOperation("B");
+            LongOperation("C");
+            LongOperation("D");
+            LongOperation("E");
+            stopwatch.Stop();
+            Console.WriteLine(" Synchronous long operation calls finished {0} sec.", stopwatch.Elapsed.TotalSeconds);
+
+            stopwatch.Restart();
+            Parallel.Invoke(() => LongOperation("A"),
+            () => LongOperation("B"),
+            () => LongOperation("C"),
+            () => LongOperation("D"),
+            () => LongOperation("E"));
+            stopwatch.Stop();
+            Console.WriteLine(" Parallel long operation calls finished {0} sec.", stopwatch.Elapsed.TotalSeconds);
+
+            stopwatch.Restart();
+            Parallel.For(0, 1000, (i) =>
+            {
+                var x = 2;
+                var y = 2;
+                var sum = x + y;
+            });
+            stopwatch.Stop();
+            Console.WriteLine(" Parallel calls finished {0} ms.", stopwatch.Elapsed.TotalMilliseconds);
+
+            stopwatch.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = 2;
+                int y = 2;
+                int sum = x + y;
+            }
+            stopwatch.Stop();
+            Console.WriteLine(" Sync operation calls finished {0} ms.", stopwatch.Elapsed.TotalMilliseconds);
+
+            int counter = 0;
+            Parallel.For(0, 100, (i) =>
+            {
+                Thread.Sleep(1);
+                counter += 1;
+            });
+            Console.WriteLine(" Counter should be 100. Counter is {0}", counter);
+
+            object lockObject = new object();
+            List<int> results = new List<int>();
+            Parallel.For(0, 100, (i) =>
+            {
+                Thread.Sleep(1);
+                lock (lockObject)
+                {
+                    results.Add(i * i);
+                }
+            });
+            Console.WriteLine("Bag length should be 100. Length is {0}", results.Count);
+
+            ConcurrentBag<int> iterations = new ConcurrentBag<int>();
+            Parallel.For(0, 100, (i) =>
+            {
+                Thread.Sleep(1);
+                iterations.Add(i);
+            });
+            Console.WriteLine("Bag length should be 100. Length is {0}", iterations.Count);
+
+        }
+
+        public static void LongOperation(string taskName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} Finished . Executing Thread : {1}", taskName,
+            Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ zad7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main method is the only method that
+            // can ’t be marked with async .
+            // What we are doing here is just a way for us to simulate
+            // async - friendly environment you usually have with
+            // other .NET application types ( like web apps , win apps etc .)
+            // Ignore main method , you can just focus on LetsSayUserClickedAButtonOnGuiMethod() as a
+            // first method in call hierarchy .
+            var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+            }
+            private static async void LetsSayUserClickedAButtonOnGuiMethod()
+            {
+                Console.WriteLine(await GetTheMagicNumber());
+            }
+            private static async Task<int> GetTheMagicNumber()
+            {
+                return await IKnowIGuyWhoKnowsAGuy();
+            }
+            private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+            {
+                return await IKnowWhoKnowsThis(10) + await IKnowWhoKnowsThis(5);
+            }
+            private static async Task<int> IKnowWhoKnowsThis(int n)
+            {
+                return await FactorialDigitSumAsync(n);
+            }
+
+
+
+
+    public static async Task<int> FactorialDigitSumAsync(int x)
+        {
+            int fact = 1;
+            int sum = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                fact *= i;
+            }
+            while (fact != 0)
+            {
+                sum += fact % 10;
+                fact = fact / 10;
+            }
+            return sum;
+        }
+    }
+  }
